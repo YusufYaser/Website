0c5415822001b6b9c8fb07ac26059ef55152c9c1
Yusuf Kelany <me@yusufyaser.xyz>
2024-07-10 17:50:22 +0300

    updates

--- src/App.js
+++ src/App.js
@@ -2,13 +2,54 @@ import { faArrowDown, faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import githubIcon from './assets/github.svg'
 import twitterIcon from './assets/twitter.svg'
-import { useEffect, useRef } from "react";
+import linkedinIcon from './assets/linkedin.png'
+import { useEffect, useRef, useState } from "react";
+import useOnScreen from './useOnScreen'
+
+function wait(time) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, time)
+    })
+}
 
 function App() {
     const bg = useRef(null)
+    const fgBlur = useRef(null)
+    const section2 = useRef(null)
+    const section2Visible = useOnScreen(section2)
+    const [titleText, setTitleText] = useState("")
+    const TITLE = "Hey there, my name is Yusuf Kelany"
 
     useEffect(() => {
-        // spawn circles
+        let lastScroll = window.scrollY
+        window.addEventListener("scroll", (e) => {
+            let delta = window.scrollY - lastScroll
+            // bg.current.style.transitionDuration = "0s"
+            // bg.current.style.top = `${-delta * 5}px`
+            fgBlur.current.style.backdropFilter = `blur(${Math.abs(delta) / 20}px)`
+            setTimeout(() => {
+                // bg.current.style.transitionDuration = "1s"
+                // bg.current.style.top = `0px`
+                fgBlur.current.style.backdropFilter = `blur(${0}px)`
+            }, 200)
+            lastScroll = window.scrollY
+        })
+    }, [])
+
+    useEffect(async () => {
+        let id
+        let curText = ""
+        await wait(1000)
+        id = setInterval(() => {
+            curText += TITLE.charAt(curText.length)
+            setTitleText(curText)
+            if (curText.length >= TITLE.length) {
+                clearInterval(id)
+            }
+        }, 50)
+    }, [])
+
+    useEffect(() => {
         for (let i = 0; i < 30; i++) {
             const circle = document.createElement("p")
             circle.className = "bgcircle"
@@ -32,11 +73,12 @@ function App() {
         <div>
             <div id="background" ref={bg}></div>
             <div id="bgblur"></div>
+            <div id="fgblur" ref={fgBlur} style={{ backdropFilter: 'blur(0px)' }}></div>
 
             <div className="section">
                 <center className="content">
-                    <h1>Hey there, my name is Yusuf Yaser!</h1>
-                    A Backend Developer and a System Administrator
+                    <h1>{titleText} <i className="cursor"></i></h1>
+                    Software Developer and System Administrator
                     <br/><br/>
                     <span>
                         <h2>
@@ -67,36 +109,60 @@ function App() {
                 </center>
             </div>*/}
 
-            <div className="section">
+            <div className="section" style={{
+                marginTop: '16rem'
+            }}>
                 <center className="content">
-                    <h1>Find and contact me</h1>
+                    <h1 ref={section2}>Find and contact me</h1>
 
                     <a
                         href={"https://github.com/YusufYaser"}
                         target={"_blank"}
                         rel={"noreferrer"}
+                        className="icon-link"
+                        draggable={false}
                     >
                         <img
                             className="icon"
                             src={githubIcon}
-                            alt={"YusufYaser's GitHub Page"}
+                            alt={"GitHub: YusufYaser"}
+                            draggable={false}
                         />
                     </a>
                     <a
-                        href={"https://twitter.com/RealYusufYaser"}
+                        href={"https://x.com/RealYusufYaser"}
                         target={"_blank"}
                         rel={"noreferrer"}
+                        className="icon-link"
+                        draggable={false}
                     >
                         <img
                             className="icon"
                             src={twitterIcon}
-                            alt={"YusufYaser's Twitter Page"}
+                            alt={"Twitter: @RealYusufYaser"}
+                            draggable={false}
+                        />
+                    </a>
+                    <a
+                        href={"https://linkedin.com/in/yusufyaser"}
+                        target={"_blank"}
+                        rel={"noreferrer"}
+                        className="icon-link"
+                        draggable={false}
+                    >
+                        <img
+                            className="icon"
+                            src={linkedinIcon}
+                            alt={"Linkedin: yusufyaser"}
+                            draggable={false}
                         />
                     </a>
                     <a
                         href={"mailto:me@yusufyaser.xyz"}
                         target={"_blank"}
                         rel={"noreferrer"}
+                        className="icon-link"
+                        draggable={false}
                     >
                         <FontAwesomeIcon
                             className="icon"
@@ -108,9 +174,7 @@ function App() {
 
             <div className="section">
                 <center className="content">
-                    unfinished, will be updated soon ;)
-                    <br/><br/>
-                    <strong>Copyright &copy; 2023 ~ Yusuf Yaser</strong>
+                    <strong>Copyright &copy; 2024 ~ Yusuf Kelany</strong>
                 </center>
             </div>
         </div>
