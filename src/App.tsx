ba45db1d7c62dbf012c8597d10571878c192b38c
Yusuf Kelany <me@yusufyaser.xyz>
2024-12-19 21:54:53 +0200

    new website initial design

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import Background from "./components/Background";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
+    <div className="flex flex-col flex-wrap space-y-24 text-white content-center">
+      <Background/>
+
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <h1 className="text-2xl font-bold">
+          Hey there, I'm <span className="text-red-600">Yusuf Kelany</span>!
+        </h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          A Software Developer and System Administrator
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <div>
+        <h1 className="text-2xl font-bold">
+          Where to <span className="text-red-600">find</span> me
+        </h1>
+        
+        <div className="grid grid-cols-3 gap-8 mt-4" id="socials">
+          <a href="https://github.com/YusufYaser" target="_blank" rel="noreferrer">
+            <img className="size-24 object-contain lg:hover:scale-125 lg:active:scale-[80%] duration-150" src="/assets/github.svg" />
+          </a>
+          <a href="https://www.linkedin.com/in/yusufyaser" target="_blank" rel="noreferrer">
+            <img className="size-24 object-contain lg:hover:scale-125 lg:active:scale-[80%] duration-150" src="/assets/linkedin.png" />
+          </a>
+          <a href="https://discord.com/users/340426742466609152" target="_blank" rel="noreferrer">
+            <img className="size-24 object-contain lg:hover:scale-125 lg:active:scale-[80%] duration-150" src="/assets/discord.svg" />
+          </a>
+          <a href="https://x.com/RealYusufYaser" target="_blank" rel="noreferrer">
+            <img className="size-24 object-contain lg:hover:scale-125 lg:active:scale-[80%] duration-150" src="/assets/twitter.svg" />
+          </a>
+        </div>
+      </div>
+
+
+      {/* bottom spacing */}
+
+      <div className="h-24"></div>
+    </div>
   )
 }
-
-export default App
---
+++ src/components/Background.tsx
@@ -0,0 +1,72 @@
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadSlim } from "@tsparticles/slim";
+import { useEffect, useState } from 'react';
+
+export default function Background() {
+  const [ init, setInit ] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true)
+    });
+  }, []);
+
+  return (
+    <>
+      {init && <Particles
+        className="particles-background"
+        style={{
+          zIndex: -100,
+          position: "absolute",
+        }}
+        options={{
+          background: {
+            color: {
+              value: "#000",
+            },
+          },
+          fpsLimit: 120,
+          particles: {
+            color: {
+              value: "#ffffff",
+            },
+            links: {
+              color: "#ffffff",
+              distance: 150,
+              enable: true,
+              opacity: 0.5
+            },
+            move: {
+              direction: "none",
+              enable: true,
+              outModes: {
+                default: "bounce",
+              },
+              random: false,
+              speed: 6,
+              straight: false,
+            },
+            number: {
+              density: {
+                enable: true
+              },
+              value: 250,
+            },
+            opacity: {
+              value: 0.5,
+            },
+            shape: {
+              type: "circle",
+            },
+            size: {
+              value: { min: 1, max: 5 },
+            },
+          },
+          detectRetina: true,
+        }}
+      />}
+    </>
+  )
+}
