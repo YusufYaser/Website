397a741ead007e6ed705ae9bc580b2abea30978a
Yusuf Kelany <me@yusufyaser.xyz>
2025-01-30 09:38:11 +0200

    add code block syntax highlighting

--- src/BlogPost.tsx
+++ src/BlogPost.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import Markdown from "react-markdown";
 import { useLocation } from "react-router";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
 import remarkGfm from "remark-gfm";
 import { getPostContent, getPostMeta } from "./Blog";
 import './Blog.css';
@@ -30,7 +32,26 @@ export default function BlogPost() {
 
   return (
     <div>
-      <Markdown className="md lg:w-1/2 lg:p-0 p-8 center w-full ml-auto mr-auto" remarkPlugins={[remarkGfm]}>
+      <Markdown
+        className="md lg:w-1/2 lg:p-0 p-8 center w-full ml-auto mr-auto"
+        remarkPlugins={[remarkGfm]}
+          components={{
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            code({ node, className, children, ...props }) {
+              const match = /language-(\w+)/.exec(className || "");
+              return match ? (
+                // @ts-expect-error style is complaining for no reason
+                <SyntaxHighlighter style={oneDark} language={match[1]} {...props}>
+                  {String(children).replace(/\n$/, "")}
+                </SyntaxHighlighter>
+              ) : (
+                <code className={className} {...props}>
+                  {children}
+                </code>
+              );
+            },
+          }}
+      >
         {content}
       </Markdown>
     </div>
